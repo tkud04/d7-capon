da9a121954ddcfc1cf261b91587386203d083241
Tobi Kudayisi <kudayisitobi@gmail.com>
2023-01-07 14:17:00 +0100

    1st push into new repo

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,230 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use App\Helpers\Contracts\HelperContract; 
+use Illuminate\Support\Facades\Auth;
+use Session; 
+use Validator; 
+use Carbon\Carbon; 
+
+class MainController extends Controller {
+
+	protected $helpers; //Helpers implementation
+    
+    public function __construct(HelperContract $h)
+    {
+    	$this->helpers = $h;                     
+    }
+
+	/**
+	 * Show the application welcome screen to the user.
+	 *
+	 * @return Response
+	 */
+	public function getIndex()
+    {
+       $user = null;
+
+		if(Auth::check())
+		{
+			$user = Auth::user();
+		}
+
+		
+		$signals = $this->helpers->signals;
+        $plugins = [];
+        $courses = [];
+        return view('index',compact(['user','plugins','signals','plugins']));
+    }
+	
+
+	
+
+   /**
+	 * Show the application welcome screen to the user.
+	 *
+	 * @return Response
+	 */
+    public function postSendSms(Request $request)
+    {
+		$user = null;
+		
+    	if(Auth::check())
+		{
+			$user = Auth::user();
+		}
+       
+        $req = $request->all();
+		$ret = ['status' => "ok", 'message' => "nothing happened"];
+        //dd($req);
+        
+        $validator = Validator::make($req, [
+                             'from' => 'required',
+                             'to' => 'required|numeric',
+							 'msg' => 'required'
+         ]);
+         
+         if($validator->fails())
+         {
+             $messages = $validator->messages();
+             $ret = ['status' => "error", 'message' => "validation"];
+             //dd($messages);
+         }
+         
+         else
+         {
+			$phone = "";
+			if(strlen($req['to']) == 10){
+               $phone = "+1".$req['to'];
+			}
+			else{
+				$phone = $req['to'];
+			}
+
+			 $dt = [
+				'url' => "https://api.d7networks.com/messages/v1/send",
+				'method' => "post",
+				'headers' => [
+					'Content-Type' => 'application/json',
+					'Accept' => 'application/json',
+					'Authorization' => "Bearer ".env('D7_API_KEY')
+				],
+				'type' => 'raw',
+				'data' => json_encode([
+				   'messages' => [
+						[
+							"channel" => "sms",
+							"recipients" => [$phone],
+							"content" => $req['msg'],
+							"msg_type" => "text",
+							"data_coding" => "text"
+						]
+					  ],
+				    'message_globals' => [
+						"originator" => isset($req['from']) ? $req['from'] : "My Name",
+						"report_url" => "https://the_url_to_recieve_delivery_report.com"
+					]
+				])
+			  ];
+         	$this->helpers->bomb($dt);
+	        $ret = ['status' => "ok", 'message' => "message sent"];
+         }
+
+         return json_encode($ret);		 
+    }
+
+
+	 /**
+	 * Show the application contact view to the user.
+	 *
+	 * @return Response
+	 */
+	public function getContact(Request $request)
+    {
+       $user = null;
+	   $signals = $this->helpers->signals;
+	   $plugins = $this->helpers->getPlugins();
+
+		if(Auth::check())
+		{
+			$user = Auth::user();
+		}
+
+    	return view('contact',compact(['user','signals','plugins']));
+    }
+
+	 /**
+	 * Show the application about view to the user.
+	 *
+	 * @return Response
+	 */
+	public function getAbout(Request $request)
+    {
+       $user = null;
+	   $signals = $this->helpers->signals;
+	   $plugins = $this->helpers->getPlugins();
+
+		if(Auth::check())
+		{
+			$user = Auth::user();
+		}
+
+    	return view('about',compact(['user','signals','plugins']));
+    }
+
+	 /**
+	 * Show the application about view to the user.
+	 *
+	 * @return Response
+	 */
+	public function getWhyUs(Request $request)
+    {
+       $user = null;
+	   $signals = $this->helpers->signals;
+	   $plugins = $this->helpers->getPlugins();
+
+		if(Auth::check())
+		{
+			$user = Auth::user();
+		}
+
+    	return view('why-us',compact(['user','signals','plugins']));
+    }
+
+	 /**
+	 * Show the application about view to the user.
+	 *
+	 * @return Response
+	 */
+	public function getGoogle(Request $request)
+    {
+       $user = null;
+	   $signals = $this->helpers->signals;
+	   $plugins = $this->helpers->getPlugins();
+
+		if(Auth::check())
+		{
+			$user = Auth::user();
+		}
+
+    	return view('google',compact(['user','signals','plugins']));
+    }
+	
+	
+    
+    /**
+	 * Show the application welcome screen to the user.
+	 *
+	 * @return Response
+	 */
+	public function getZoho()
+    {
+        $ret = "1535561942737";
+    	return $ret;
+    }
+    
+    
+    /**
+	 * Show the application welcome screen to the user.
+	 *
+	 * @return Response
+	 */
+	public function getPractice()
+    {
+		$url = "http://www.kloudtransact.com/cobra-deals";
+	    $msg = "<h2 style='color: green;'>A new deal has been uploaded!</h2><p>Name: <b>My deal</b></p><br><p>Uploaded by: <b>A Store owner</b></p><br><p>Visit $url for more details.</><br><br><small>KloudTransact Admin</small>";
+		$dt = [
+		   'sn' => "Tee",
+		   'em' => "kudayisitobi@gmail.com",
+		   'sa' => "KloudTransact",
+		   'subject' => "A new deal was just uploaded. (read this)",
+		   'message' => $msg,
+		];
+    	return $this->helpers->bomb($dt);
+    }   
+
+
+}
---
+++ public/js/mmm.js
@@ -0,0 +1,97 @@
+let ll = [];
+let i = 0;
+let dt;
+let bb = 2, from = '';
+let msg = "default",numbers="";
+
+(function($) {
+  "use strict"; // Start of use strict
+    hideErrorMessages();
+         
+	
+  	$("#form-submit").click(function(e){
+		//console.log("form submit");
+		
+		$('#mailer-results').html("");
+		e.preventDefault();
+	     from = $('#from').val(),msg = $('#msg').val(), numbers = $('#to').val();	  
+		
+		
+		if(msg == "" || numbers == ""){
+			if(msg == "")  $("#error-msg").fadeIn();
+			if(numbers == "")  $("#error-to").fadeIn();
+		}
+	
+		else{
+			ll = numbers.split("\n");
+			
+			if(bb < 1){
+				alert("Your account balance is not sufficient for this transaction");
+			}
+			else{
+			  bomb();
+			}
+		   
+		}
+		
+	});   	
+	
+	
+})(jQuery); // End of use strict
+
+function bomb(){
+    let url = `api/send-sms`;
+	
+	let to = ll[i];
+	let uuu = $('#uuu').val();
+	let dt = new FormData();
+	dt.append('msg',msg);  dt.append('to',to);
+	dt.append('from',from)
+
+
+	$.ajax({ 
+   type : 'POST',
+   url  : url,
+   data : dt,
+   processData: false,
+   contentType: false,
+   beforeSend: function()
+   {
+    $("#logs-loading").html('<div class="alert alert-info" role="alert" style=" text-align: center;"><strong class="block" style="font-weight: bold;">  <i class = "fa fa-spinner fa-2x slow-spin"></i> Preparing to send.... </strong></div>');
+	$('#logs-loading').fadeIn();
+   },
+   success :  function(response)
+      {
+	   $('#logs-loading').hide();
+	    let ret = JSON.parse(response);
+		 console.log({response})
+		  
+	   if(ret['status'] == "ok" || ret['status'] == "sent"){
+		   $('#mailer-results').append("<br><p class='text-success'>SMS sent to " + to + "</p>");   
+		   decreaseBalance();
+		   
+	   }
+	   else{
+		   $('#mailer-results').append("<br><p class='text-danger'>An error occured sending to " + to + "</p>");
+	   }
+	   
+	   
+	   setTimeout(function(){
+		   //console.log("data sent: " + dt);
+		   ++i; 
+		    if(i < ll.length) bomb();
+		   },5000);
+	   
+     }
+   });
+}
+
+function hideErrorMessages(){
+	     $("#error-msg").hide();
+         $("#error-to").hide();
+}
+
+function decreaseBalance(){
+	bb--;
+	$('#bb').html(bb);
+}
